16aeee587a51b98041e250831b9208d945f7ee8a
Stefanalesi <113575852+Stefanalesi@users.noreply.github.com>
2024-11-03 17:29:46 +0100

    why can i not commit and push normally this is annoying

---
+++ src/app/game-component/status-component/status-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-status-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './status-component.component.html',
+  styleUrl: './status-component.component.scss'
+})
+export class StatusComponentComponent {
+
+}
