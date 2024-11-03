69c6f3fd0c25cc3a0eab71e58b3732a50ff7a6af
Stefanalesi <113575852+Stefanalesi@users.noreply.github.com>
2024-11-03 17:28:00 +0100

    added the needed components for later

---
+++ game-component/cell-component/cell-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cell-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './cell-component.component.html',
+  styleUrl: './cell-component.component.scss'
+})
+export class CellComponentComponent {
+
+}
